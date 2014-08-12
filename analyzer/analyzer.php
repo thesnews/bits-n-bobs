f322e33a17d3238451c63d8a43b56d5924de2715
Mike Joseph <mike.joseph5@gmail.com>
2014-08-12 14:50:36 +0000

    added created|timeSince filter cleaned up error display cleaned up colors

--- analyzer/analyzer.php
+++ analyzer/analyzer.php
@@ -1,5 +1,7 @@
 <?php
 
+// find article.created|timeSince
+
 require 'vendor/autoload.php';
 
 $strict = in_array('--strict', $_SERVER['argv']);
@@ -34,8 +36,10 @@ $i = $finder
 
 // $spinner = new \cli\notify\Spinner('Checking '.$path);
 
-$errors = false;
+$is_clean = true;
 foreach( $i as $file ) {
+    $errors = array();
+
     // $spinner->tick();
     $continue = false;
 
@@ -64,6 +68,15 @@ foreach( $i as $file ) {
     $j = 0;
     foreach( $lines as $line ) {
         $j++;
+
+        if( strpos($line, '.created|timeSince') !== false ) {
+            $is_clean = false;
+            $errors[] = array(
+                'message'   => "Displaying created time, should be using published time",
+                'line'      => $j
+            );
+        }
+
         if( strpos($line, '{% fetch') !== false ) {
             $in_fetch = true;
             $found_status = false;
@@ -97,19 +110,31 @@ foreach( $i as $file ) {
         }
 
         if( $in_fetch && strpos($line, '%}') !== false ) {
-            if( ($in_fetch_article || $in_fetch_media || $in_fetch_post) && !$found_status ) {
-                $errors = true;
-                \cli\line('%C%1Missing Status%n: '.$file->getRealPath().':'.$j);
-            }
+            if( $in_fetch_article || $in_fetch_media || $in_fetch_post ) {
 
-            if( ($in_fetch_article || $in_fetch_media || $in_fetch_post) && $found_created ) {
-                $errors = true;
-                \cli\line('%C%1Using create sorting%n: '.$file->getRealPath().':'.$j);
-            }
+                if( !$found_status ) {
+                    $is_clean = false;
+                    $errors[] = array(
+                        'message'   => "Missing 'status = 1' filter",
+                        'line'      => $j
+                    );
+                }
 
-            if( ($in_fetch_article || $in_fetch_media || $in_fetch_post) && $found_pub_after_weight ) {
-                $errors = true;
-                \cli\line('%C%1Weight should be before date%n: '.$file->getRealPath().':'.$j);
+                if( $found_created ) {
+                    $is_clean = false;
+                    $errors[] = array(
+                        'message'   =>  "Using create time sorting, should be using published time",
+                        'line'      => $j
+                    );
+                }
+
+                if( $found_pub_after_weight ) {
+                    $is_clean = false;
+                    $errors[] = array(
+                        'message'   => "Weight order sorting should be before date order sorting",
+                        'line'      => $j
+                    );
+                }
             }
 
             $in_fetch = false;
@@ -121,6 +146,15 @@ foreach( $i as $file ) {
             $found_pub_after_weight = false;
         }
     }
+
+    if( count($errors) ) {
+        \cli\line("");
+        \cli\line("\33[1;31m".$file->getRealPath()."%n");
+        foreach( $errors as $error ) {
+            \cli\line(sprintf("\33[0;33mLine %s: %s%%n", $error['line'], $error['message']));
+        }
+        \cli\line("");
+    }
 }
 
 // $spinner->finish();
@@ -128,10 +162,10 @@ foreach( $i as $file ) {
 \cli\line('Scan complete');
 
 \cli\line("");
-if( $errors ) {
-    \cli\line("%C%1\n\n Dang. Errors found.\n\n%n");
+if( !$is_clean ) {
+    \cli\line("\33[1;31mDang. Errors found.%n");
 } else {
-    \cli\line("%C%2\n\n Yay, it's clean!\n\n%n");
+    \cli\line("\33[0;32mYay, it's clean!%n");
 }
 \cli\line("");
 // file_put_contents('./analyzer-results.txt', implode("\n", $errors));
